12b93080b577ac977400a32e1fd53af435b06871
matt <mattevo10@gmail.com>
2022-01-05 17:27:05 +0100

    styling added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import Article from "./components/Article";
+import NoArticles from "./components/NoArticles";
+
 
 function App() {
   const [articles, setArticles] = useState([]);
-  const [subreddit, setSubreddit] = useState('OnePiece');
+  const [subreddit, setSubreddit] = useState('');
 
-  useEffect(() => {
-    fetch("https://www.reddit.com/r/OnePiece.json").then(res =>{
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://www.reddit.com/r/${subreddit}.json`).then(res =>{
+      if (res.status !== 200) {
+          console.log("Error");
+          return;
+        }
       res.json().then(data => {
         if (data != null) {
-          console.log(data)
+          setArticles(data.data.children)
         }
       })
     })
-  }, [subreddit])
+  }
+
   return (
     <div className="App">
       <header>
-        <input type="text" className="input" defaultValue="OnePiece" />
+        <form onSubmit={handleSubmit}>
+          <input className="subreddit_input" type="text" placeholder="Find a subreddit" name="hello" value={subreddit} onChange={e =>setSubreddit(e.target.value)}/>
+        </form>
       </header>
       <div className="articles">
-
+        {
+          (articles.length > 0) ? articles.map((article,i) => <Article article={article.data} key={i} />) : <NoArticles/>
+        }
       </div>
     </div>
   );
---
+++ src/components/NoArticles.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NoArticles() {
+  return (
+    <div className='noArticles'>
+      <h1>Search Example</h1>
+      <li>soccer</li>
+      <li>onepiece</li>
+      <li>hunterxhunter</li>
+      <li>climbing</li>
+      <li>berlin</li>
+    </div>
+  )
+}
+
+export default NoArticles
